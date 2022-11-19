99801abb2b0a1c28dcacee756986e69cbc28dc1f
Christopher Jenkin <christopherjenkin64@gmail.com>
2022-11-19 11:34:05 -0500

    command testing

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 import os
+from discord import app_commands
 from discord.ext.commands import Bot
 from datetime import datetime
 import random
@@ -9,11 +10,17 @@ intents.message_content = True
 intents.reactions = True
 intents.members = True
 
+tree = app_commands.CommandTree(client)
 overwatchHeroTankList = ["D.VA", "Doomfist", "Junkerqueen","Orisa","Reinhardt","Roadhod","Sigma","Winston","Wrecking Ball","Zarya"]
 overwatchHeroSupportList = ["Ashe", "Bastion", "Cassidy","Echo","Genji","Hanzo","Junkrat","Mei","Pharah","Reaper","Sojourn","Soldier 76","Sombra(Please Dont)","Symmetra","Torbjorn","Tracer","Widowmaker"]
 overwatchHeroDPSList = ["Ana", "Baptiste", "Brigitte","Kiriko","Lucio","Mercy","Moira","Zenyatta"]
 client = discord.Client(intents=intents)
 bot = Bot("!",intents=intents)
+
+@tree.command(name = "Hello", description = "hello command", guild=discord.Object(id=222147212681936896)) #Add the guild ids in which the slash command will appear. If it should be in all, remove the argument, but note that it will take some time (up to an hour) to register the command if it's for all guilds.
+async def first_command(interaction):
+    await interaction.response.send_message("Hello!")
+
 @bot.command()
 async def Ping(ctx):
     await ctx.send(f"Pong! {round(client.latency * 1000)}ms")
