29d7e87f3baa2c6ffc894579f1dc3218eff9b509
Christopher Jenkin <christopherjenkin64@gmail.com>
2022-12-09 12:59:28 -0500

    added Ramattra!!

--- bot.py
+++ bot.py
@@ -11,7 +11,7 @@ intents.reactions = True
 intents.members = True
 
 client = discord.Client(intents=intents)
-overwatchHeroTankList = ["D.VA", "Doomfist", "Junkerqueen","Orisa","Reinhardt","Roadhod","Sigma","Winston","Wrecking Ball","Zarya"]
+overwatchHeroTankList = ["D.VA", "Doomfist", "Junkerqueen","Orisa","Reinhardt","Roadhod","Sigma","Winston","Wrecking Ball","Zarya","Ramattra"]
 overwatchHeroDPSList = ["Ashe", "Bastion", "Cassidy","Echo","Genji","Hanzo","Junkrat","Mei","Pharah","Reaper","Sojourn","Soldier 76","Sombra(Please Dont)","Symmetra","Torbjorn","Tracer","Widowmaker"]
 overwatchHeroSupportList = ["Ana", "Baptiste", "Brigitte","Kiriko","Lucio","Mercy","Moira","Zenyatta"]
 overwatchRoleList = ["Tank", "DPS", "Support"]
