36d2665c44cfd915c170f5ce645e9be3dff07c24
Christopher Jenkin <christopherjenkin64@gmail.com>
2023-05-05 16:28:19 -0400

    added a check for ggez

--- bot.py
+++ bot.py
@@ -121,6 +121,8 @@ async def on_message(message):
 
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    if  message.content.contains('ggez'):
+        await message.channel.send(random.choice(["Well played. I salute you all.","For glory and honor! Huzzah comrades!","I'm wrestling with some insecurity issues in my life but thank you all for playing with me.","It's past my bedtime. Please don't tell my mommy.","Gee whiz! That was fun. Good playing!","I feel very, very small... please hold me..."]))
 saved_messages = {}
 @client.event
 async def on_raw_reaction_add(payload: discord.RawReactionActionEvent):
