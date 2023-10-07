d64437e56c2da7648664d9c87893eb8d95b0bb18
Christopher Jenkin <christopherjenkin64@gmail.com>
2023-10-07 10:38:36 -0400

    feat: added in randomness for local or api

--- logic.py
+++ logic.py
@@ -1,7 +1,6 @@
 import random
 from pets import RandomCatAPIPet, RandomPet
 def ChooseLocalOrApi():
-    return RandomCatAPIPet()
-    if ( random() >= .5 ):
+    if ( random.random() >= .5 ):
         return RandomPet()
     return RandomCatAPIPet()
