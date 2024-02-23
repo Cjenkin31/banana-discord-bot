4b7047c37ebba91dac1fbb62469fb1fe3f054904
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-02-22 21:31:22 -0500

    updated to use nickname

--- bot.py
+++ bot.py
@@ -61,8 +61,8 @@ async def on_voice_state_update(member, before, after):
     if after.channel and after.channel.name == "Join To Create VC":
         guild = after.channel.guild
         category = after.channel.category
-
-        new_channel = await guild.create_voice_channel(name=f"{member.name}'s VC", category=category)
+        channel_name = f"{member.display_name}'s VC"
+        new_channel = await guild.create_voice_channel(name=channel_name, category=category)
 
         await member.move_to(new_channel)
 
