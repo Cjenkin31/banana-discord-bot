05552ec146b585bc3cb379d15a2f4a79bf4c632d
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-02-22 20:32:56 -0500

    fixed tree sync

--- bot.py
+++ bot.py
@@ -44,9 +44,10 @@ def CreateEmbedMessage(message):
 @client.event
 async def on_ready():
     print(f'We have logged in as {client.user}')
-    await tree.sync(guild=discord.Object(id=mainServerId))
-    await tree.sync(guild=discord.Object(id=sideServerId))
+    await tree.sync(guild=discord.Object(id="222147212681936896"))
+    await tree.sync(guild=discord.Object(id="1101665956314501180"))
     print("Ready!")
+
 
 @client.event
 async def on_message(message):
