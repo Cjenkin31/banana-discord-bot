69fcf39e142eb38c223d05f00b8f8c8f1f3b5ae8
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-02-26 20:14:47 -0500

    changed bread voice

--- commands.py
+++ commands.py
@@ -107,7 +107,8 @@ def DefineAllCommands(tree):
             }
         }
         # Female Bread: Iq2WyJggqdxjND19FFJw
-        url = "https://api.elevenlabs.io/v1/text-to-speech/Iq2WyJggqdxjND19FFJw"
+        # Female Bread2: saUfe5jyFdcsZbN5Yt1c
+        url = "https://api.elevenlabs.io/v1/text-to-speech/saUfe5jyFdcsZbN5Yt1c"
         response = requests.post(url, json=data, headers=headers)
 
         file_path = 'temp_response.mp3'
