8e5a12c6ce00a3860b51ae927a52e23a921f8a46
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 18:00:31 -0400

    more awaits

--- events/ready.py
+++ events/ready.py
@@ -1,15 +1,14 @@
 import discord
 from discord.ext import commands
-import os
-from discord import app_commands
-from discord.ext.commands import Bot
 from Commands.setup_commands import define_all_commands
 from config.config import SERVERS
+from events.setup_events import setup_events
 
 async def setup_ready(bot):
     @bot.event
     async def on_ready():
         print(f'Logged in as {bot.user}!')
         await define_all_commands(bot, SERVERS)
+        await setup_events(bot)
         for guild in SERVERS:
             await bot.tree.sync(guild=guild)
