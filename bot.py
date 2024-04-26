6fb3e113798a672356a41e8d0d83556ab56edddd
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 18:02:15 -0400

    more async

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
 import discord
 from discord.ext import commands
-from config.config import TOKEN, INTENTS, SERVERS
+import asyncio
+from config.config import TOKEN, INTENTS
 from events.ready import setup_ready
 from utils.error_handlers import setup_logging
-from Commands.setup_commands import define_all_commands
+
 bot = commands.Bot(command_prefix="!", intents=INTENTS)
 
-setup_ready(bot)
-setup_logging()
+async def main():
+    await setup_ready(bot)
+    setup_logging()
+    bot.run(TOKEN)
 
-bot.run(TOKEN)
+if __name__ == "__main__":
+    asyncio.run(main())  # Run the main function to start everything
