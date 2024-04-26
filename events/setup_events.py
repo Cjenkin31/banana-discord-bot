638bff337f60a86afe2f26029076bef88214fe2e
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 17:28:16 -0400

    update import

--- events/setup_events.py
+++ events/setup_events.py
@@ -4,10 +4,10 @@ import os
 from discord import app_commands
 from discord.ext.commands import Bot
 
-from message import setup_message
-from reaction_add import setup_reaction_add
-from ready import setup_ready
-from voice_state_update import setup_voice_state_update
+from .message import setup_message
+from .reaction_add import setup_reaction_add
+from .ready import setup_ready
+from .voice_state_update import setup_voice_state_update
 
 def setup_events(bot):
     setup_ready(bot)
