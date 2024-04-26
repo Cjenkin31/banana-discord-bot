c920be3572c55720d3a4a5f349d37a3598bb67cf
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 18:00:38 -0400

    change setup

--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 import discord
 from discord.ext import commands
 from config.config import TOKEN, INTENTS, SERVERS
-from events.setup_events import setup_events
+from events.ready import setup_ready
 from utils.error_handlers import setup_logging
 from Commands.setup_commands import define_all_commands
 bot = commands.Bot(command_prefix="!", intents=INTENTS)
 
-setup_events(bot)
+setup_ready(bot)
 setup_logging()
 
 bot.run(TOKEN)
