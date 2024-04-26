d115dc1b90de0f66a8d42293ab755a13e3bf112c
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 18:04:41 -0400

    looping setup bug

--- events/setup_events.py
+++ events/setup_events.py
@@ -6,11 +6,9 @@ from discord.ext.commands import Bot
 
 from .message import setup_message
 from .reaction_add import setup_reaction_add
-from .ready import setup_ready
 from .voice_state_update import setup_voice_state_update
 
 async def setup_events(bot):
-    await setup_ready(bot)
     await setup_message(bot)
     await setup_voice_state_update(bot)
     await setup_reaction_add(bot)
