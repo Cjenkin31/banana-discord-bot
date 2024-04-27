b75313db869c4b784496df79a8b4f84ccce4d84d
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 23:32:17 -0400

    correct update

--- bot.py
+++ bot.py
@@ -7,13 +7,15 @@ from events.ready import setup_ready
 from utils.error_handlers import setup_logging
 
 bot = commands.Bot(command_prefix="!", intents=INTENTS)
-client = discord.Client(intents=INTENTS)
 
 async def main():
-    tree = app_commands.CommandTree(client)
+    tree = app_commands.CommandTree(bot)
+
     await setup_ready(bot, tree)
     setup_logging()
+
+    await tree.sync()
     await bot.start(TOKEN)
 
 if __name__ == "__main__":
-    asyncio.run(main())  # Run the main function to start everything
+    asyncio.run(main())
