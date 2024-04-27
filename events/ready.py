81528940bee19c8ec0f4638dec7e51d0d97e6a34
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 23:04:09 -0400

    more debug

--- events/ready.py
+++ events/ready.py
@@ -3,15 +3,18 @@ from discord.ext import commands
 from Commands.setup_commands import define_all_commands
 from config.config import SERVERS
 from events.setup_events import setup_events
-
 async def setup_ready(bot):
     @bot.event
     async def on_ready():
         print(f'Logged in as {bot.user}!')
         await setup_events(bot)
         await define_all_commands(bot, SERVERS)
-        try:
-            for guild in SERVERS:
+        
+        # Sync commands with each guild
+        for guild in SERVERS:
+            try:
                 await bot.tree.sync(guild=guild)
-        except:
-            print(f"Failed to sync commands with guild: {guild.id}, error: {e}")
+                print(f"Commands synced successfully with guild: {guild.id}")
+            except Exception as e:
+                print(f"Failed to sync commands with guild: {guild.id}, error: {e}")
+
