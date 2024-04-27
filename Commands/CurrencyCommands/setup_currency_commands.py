317f12df1362e382823a81bcad9a2437ed63b768
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-27 19:24:19 -0400

    defined daily command

--- Commands/CurrencyCommands/setup_currency_commands.py
+++ Commands/CurrencyCommands/setup_currency_commands.py
@@ -1,5 +1,6 @@
 from .admin_add_currency_command import define_admin_add_currency_command
 from .get_currency_command import define_get_currency_command
+from .daily_currency_command import define_daily_command
 import discord
 from discord.ext import commands
 from discord import app_commands
@@ -7,3 +8,4 @@ from discord import app_commands
 async def define_all_currency_commands(tree, servers):
     define_admin_add_currency_command(tree, servers)
     define_get_currency_command(tree, servers)
+    define_daily_command(tree, servers)
