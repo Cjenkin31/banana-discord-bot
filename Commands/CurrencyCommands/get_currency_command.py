fa68d6504eed67c0285b177b545b37b24eeadda4
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-27 19:07:53 -0400

    updated get currency command

--- Commands/CurrencyCommands/get_currency_command.py
+++ Commands/CurrencyCommands/get_currency_command.py
@@ -6,9 +6,9 @@ from data.currency import get_bananas
 def define_get_currency_command(tree, servers):
     @tree.command(name="getcurrency", description="Get your currency amount", guilds=servers)
     @app_commands.checks.has_permissions(administrator=True)
-    async def get_currency(interaction: discord.Interaction, user: discord.User, amount: int):
+    async def get_currency(interaction: discord.Interaction):
         try:
-            await get_bananas(str(user.id))
+            amount = await get_bananas(str(interaction.user.id))
             await interaction.response.send_message(f"You have {amount} bananas.")
         except Exception as e:
             await interaction.response.send_message(f"Failed to get currency: {str(e)}")
