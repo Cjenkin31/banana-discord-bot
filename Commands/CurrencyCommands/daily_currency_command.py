7a4d1cbfec813a1191340535e6520950dc6aba9a
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-27 19:23:57 -0400

    added daily command

---
+++ Commands/CurrencyCommands/daily_currency_command.py
@@ -0,0 +1,18 @@
+from data.daily import try_collect_daily
+from discord.ext import commands
+from discord import app_commands
+import discord
+
+async def define_daily_command(tree, servers):
+    @tree.command(name="daily", description="Collect your daily bananas", guilds=servers)
+    async def daily(interaction: discord.Interaction):
+        user_id = str(interaction.user.id)
+        can_collect, result = await try_collect_daily(user_id)
+        
+        if not can_collect:
+            wait_time = result
+            formatted_wait_time = f"{wait_time.seconds // 3600} hours and {(wait_time.seconds // 60) % 60} minutes"
+            await interaction.response.send_message(f"Please wait {formatted_wait_time} to collect your daily bananas.")
+        else:
+            bananas_collected = result
+            await interaction.response.send_message(f"You collected {bananas_collected} bananas! Come back in 24 hours for more.")
---
+++ data/daily.py
@@ -0,0 +1,40 @@
+from data.currency import add_bananas, get_bananas
+import firebase_admin
+import os
+import json
+from firebase_admin import credentials, db
+from datetime import datetime, timedelta, timezone
+import random
+
+service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
+
+cred = credentials.Certificate(service_account_info)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': os.getenv("FIREBASE_DATABASE_URL")
+})
+
+async def get_last_daily(user_id):
+    ref = db.reference(f'users/{user_id}/last_daily')
+    return ref.get()
+
+async def update_last_daily(user_id):
+    ref = db.reference(f'users/{user_id}')
+    ref.update({'last_daily': datetime.now(timezone.utc).isoformat()})
+
+async def try_collect_daily(user_id):
+    try:
+        last_daily_str = await get_last_daily(user_id)
+        last_daily = datetime.fromisoformat(last_daily_str) if last_daily_str else None
+        now = datetime.now(timezone.utc)
+        
+        if last_daily and (now - last_daily < timedelta(days=1)):
+            return False, (last_daily + timedelta(days=1)) - now
+        
+        bananas_to_add = random.randint(1, 100)
+        current_bananas = get_bananas(user_id)
+        await add_bananas(user_id, bananas_to_add)
+        await update_last_daily(user_id)
+        return True, bananas_to_add
+    except Exception as e:
+        print(f"Error while collecting daily bananas: {e}")
+        return None, "An error occurred. Please try again later."
