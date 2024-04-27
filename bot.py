02b6a5eded7b1f1fde5e1ee94c91d8542a62f328
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 23:29:46 -0400

    wrong tree

--- bot.py
+++ bot.py
@@ -1,14 +1,16 @@
 import discord
 from discord.ext import commands
+from discord import app_commands
 import asyncio
 from config.config import TOKEN, INTENTS
 from events.ready import setup_ready
 from utils.error_handlers import setup_logging
 
 bot = commands.Bot(command_prefix="!", intents=INTENTS)
+client = discord.Client(intents=INTENTS)
 
 async def main():
-    tree = discord.app_commands.CommandTree(bot)
+    tree = app_commands.CommandTree(client)
     await setup_ready(bot, tree)
     setup_logging()
     await bot.start(TOKEN)
