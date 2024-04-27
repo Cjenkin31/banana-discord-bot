96228ee99fa66b4f42dc3b9c6538f1ff2c83fcf1
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-27 19:15:10 -0400

    defined gambling commands

--- Commands/GamblingCommands/setup_gambling_commands.py
+++ Commands/GamblingCommands/setup_gambling_commands.py
@@ -1,5 +1,7 @@
+from .coinflip_command import define_coinflip_command
 import discord
 from discord.ext import commands
 from discord import app_commands
 
 async def define_all_gambling_commands(tree, servers):
+    define_coinflip_command(tree, servers)
