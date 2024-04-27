a54b168cdfc21c1723a44e516a62518576317caf
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-26 23:37:34 -0400

    moved token outside of main

--- bot.py
+++ bot.py
@@ -1,26 +1,20 @@
+from Commands.setup_commands import define_all_commands
 import discord
 from discord.ext import commands
 from discord import app_commands
 import asyncio
 from config.config import SERVERS, TOKEN, INTENTS
 from events.ready import setup_ready
+from events.setup_events import setup_events
 from utils.error_handlers import setup_logging
 
 bot = commands.Bot(command_prefix="!", intents=INTENTS)
 
 async def main():
-    tree = app_commands.CommandTree(bot)
-
-    await setup_ready(bot, tree)
-    setup_logging()
-
-    for guild in SERVERS:
-        try:
-            await tree.sync(guild=guild)
-            print(f"Commands synced successfully with guild: {guild.id}")
-        except Exception as e:
-            print(f"Failed to sync commands with guild: {guild.id}, error: {e}")
-    await bot.start(TOKEN)
+    await bot.wait_until_ready()
+    await setup_ready(bot, bot.tree)
+    await bot.tree.sync()
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    bot.loop.run_until_complete(main())
+    bot.run(TOKEN)
