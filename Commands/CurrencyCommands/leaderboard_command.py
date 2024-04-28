bd04aa54484c913179bfb6a29abee6b38be67820
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-28 10:58:01 -0400

    updated to use the coin

--- Commands/CurrencyCommands/leaderboard_command.py
+++ Commands/CurrencyCommands/leaderboard_command.py
@@ -9,10 +9,10 @@ async def define_leaderboard_command(tree, servers):
         try:
             leaderboard_data = await get_leaderboard()
             embed = discord.Embed(title="Top Users by Bananas", description="Here are the top banana earners:", color=0xf1c40f)
-            embed.set_thumbnail(url="https://github.com/Cjenkin31/banana-discord-bot/blob/main/images/bananaleaderboard.png")
+            embed.set_thumbnail(url="https://github.com/Cjenkin31/banana-discord-bot/blob/41392ca3e636bc870515d9ecad84e6a623485d23/images/bananaleaderboard.png")
             for index, (user_id, amount) in enumerate(leaderboard_data[:10], start=1):  # Show top 10 users
                 user = await interaction.client.fetch_user(user_id)  # Fetch user info from Discord
-                embed.add_field(name=f"{index}. {user.display_name}", value=f"{amount} bananas", inline=False)
+                embed.add_field(name=f"{index}. {user.display_name}", value=f"{amount} bananas :bananacoin:", inline=False)
             await interaction.response.send_message(embed=embed)
         except Exception as e:
             await interaction.response.send_message(f"Failed to display leaderboard: {str(e)}", ephemeral=True)
