045b7fa398bb8142e3199a87648d7d5ca90bad8e
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-28 10:49:42 -0400

    updated leaderboard

--- Commands/CurrencyCommands/leaderboard_command.py
+++ Commands/CurrencyCommands/leaderboard_command.py
@@ -8,10 +8,11 @@ async def define_leaderboard_command(tree, servers):
     async def leaderboard(interaction: discord.Interaction):
         try:
             leaderboard_data = await get_leaderboard()
-            message = "Top users by bananas:\n"
+            embed = discord.Embed(title="Top Users by Bananas", description="Here are the top banana earners:", color=0xf1c40f)
+            embed.set_thumbnail(url="https://github.com/Cjenkin31/banana-discord-bot/blob/main/images/bananaleaderboard.png")
             for index, (user_id, amount) in enumerate(leaderboard_data[:10], start=1):  # Show top 10 users
                 user = await interaction.client.fetch_user(user_id)  # Fetch user info from Discord
-                message += f"{index}. `<@{user_id}>` {user.display_name}: {amount} bananas\n"
-            await interaction.response.send_message(message)
+                embed.add_field(name=f"{index}. {user.display_name}", value=f"{amount} bananas", inline=False)
+            await interaction.response.send_message(embed=embed)
         except Exception as e:
-            await interaction.response.send_message(f"Failed to display leaderboard: {str(e)}")
+            await interaction.response.send_message(f"Failed to display leaderboard: {str(e)}", ephemeral=True)
