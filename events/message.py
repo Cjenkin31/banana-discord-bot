7d8494670d64b7f82cb72f68ef9069e99ead4579
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-28 12:13:24 -0400

    increased chances to get bananas

--- events/message.py
+++ events/message.py
@@ -23,7 +23,7 @@ async def setup_message(bot):
             ]))
         if "🍞" in message.content:
             await message.add_reaction("🍞")
-        if random.randint(1, 1000) == 1:
+        if random.randint(1, 100) == 1:
             banana_amount = random.randint(1,50)
             await add_bananas(message.author.id, banana_amount)
             await message.channel.send(f"You just found {banana_amount} {BANANA_COIN_EMOJI}")
