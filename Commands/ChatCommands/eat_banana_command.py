c29bbd06a52825f5a65afb08f3c67e9f9ca7244e
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-30 17:46:10 -0400

    bananas now scale with how many you give it

--- Commands/ChatCommands/eat_banana_command.py
+++ Commands/ChatCommands/eat_banana_command.py
@@ -11,13 +11,18 @@ async def define_eat_banana_command(tree, servers):
         banana_gif = "https://tenor.com/view/effy-gif-11375717773991506810"
         if random.randint(1, 10) == 1:
             await interaction.response.send_message(f"You decide to give {amt} banana(s) to the bot instead 🎁!\n{banana_gif}")
-            add_bananas(1011022296741326910, amt)
-            remove_bananas(interaction.user.id, amt)
+            await add_bananas(1011022296741326910, amt)
+            await remove_bananas(interaction.user.id, amt)
         else:
+            emoji_banana_amount = 0 
+            banana_string = ""
+            while emoji_banana_amount < amt:
+                banana_string+="🍌"
+                emoji_banana_amount+=1
             if amt == 1:
-                message = f"You eat {amt} banana 🍌!"
+                message = f"You eat {amt} banana {banana_string}!"
             else:
-                message = f"You eat {amt} bananas 🍌🍌!"
-            remove_bananas(interaction.user.id, amt)
+                message = f"You eat {amt} bananas {banana_string}!"
+            await remove_bananas(interaction.user.id, amt)
             await interaction.response.send_message(f"{message}\n{banana_gif}")
 
