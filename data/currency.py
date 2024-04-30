7e328adc5df50d2dd42d60ee7ffa61edbb541bd6
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-30 14:38:37 -0400

    added cacheing to the get bananas to reduce db calls

--- data/currency.py
+++ data/currency.py
@@ -3,27 +3,30 @@ import json
 import firebase_admin
 from firebase_admin import credentials, db
 import config.firebase_config
+from functools import lru_cache
 
-async def get_bananas(user_id):
+@lru_cache(maxsize=128)
+def get_bananas_sync(user_id):
     ref = db.reference(f'users/{user_id}/bananas')
     return ref.get() or 0
+
+async def get_bananas(user_id):
+    return get_bananas_sync(user_id)
 
 async def add_bananas(user_id, amount):
     ref = db.reference(f'users/{user_id}')
     current_bananas = await get_bananas(user_id)
-    ref.update({'bananas': current_bananas + amount})
+    new_bananas = current_bananas + amount
+    ref.update({'bananas': new_bananas})
+    get_bananas_sync.cache_clear()
 
 async def remove_bananas(user_id, amount):
     ref = db.reference(f'users/{user_id}')
     current_bananas = await get_bananas(user_id)
     new_bananas = max(0, current_bananas - amount)
     ref.update({'bananas': new_bananas})
+    get_bananas_sync.cache_clear()
 
-
-def set_bananas(user_id, amount):
-    ref = db.reference(f'users/{user_id}')
-    current_bananas = get_bananas(user_id)
-    ref.update({'bananas': amount})
 
 async def get_leaderboard():
     ref = db.reference('users/')
