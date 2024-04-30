aea14a8ed2f3f5b3b293106f35853ffd95f9d60b
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-04-30 17:59:35 -0400

    upated to send 2 messages

--- Commands/ChatCommands/eat_banana_command.py
+++ Commands/ChatCommands/eat_banana_command.py
@@ -8,19 +8,23 @@ import random
 from utils.emoji_helper import BANANA_COIN_EMOJI 
 
 async def define_eat_banana_command(tree, servers):
-    @tree.command(name="eat_banana", description="monkie eat", guilds=servers)
+    @tree.command(name="eat_banana", description="Monkie eat", guilds=servers)
     async def eat_banana(interaction: discord.Interaction, amt: int = 1):
         banana_gif = "https://tenor.com/view/effy-gif-11375717773991506810"
         user_id = interaction.user.id
         user_banana = await get_bananas(user_id)
+
         if amt > 500:
             amt = 500
-            await interaction.response.send_message("YOU HAVE BEEN LIMITED TO 1000 BANANAS ( Thanks to <@407302203070742529> )")
-        if user_banana < amt:
-            await interaction.response.send_message(f"YOU DON'T HAVE ENOUGH {BANANA_COIN_EMOJI} TO EAT")
+            await interaction.response.send_message("YOU HAVE BEEN LIMITED TO 500 BANANAS ( Thanks to <@407302203070742529> )")
+        elif user_banana < amt:
+            if interaction.response.is_done():
+                await interaction.followup.send(f"YOU DON'T HAVE ENOUGH {BANANA_COIN_EMOJI} TO EAT")
+            else:
+                await interaction.response.send_message(f"YOU DON'T HAVE ENOUGH {BANANA_COIN_EMOJI} TO EAT")
             return
-        if random.randint(1, 10) == 1:
-            await interaction.response.send_message(f"You decide to give {amt} banana(s) to the bot instead 🎁!\n{banana_gif}")
+        elif random.randint(1, 10) == 1:
+            await interaction.followup.send(f"You decide to give {amt} banana(s) to the bot instead 🎁!\n{banana_gif}")
             await add_bananas(1011022296741326910, amt)
             await remove_bananas(user_id, amt)
             return
@@ -28,13 +32,16 @@ async def define_eat_banana_command(tree, servers):
             emoji_banana_amount = 0 
             banana_string = ""
             while emoji_banana_amount < amt:
-                banana_string+="🍌"
-                emoji_banana_amount+=1
+                banana_string += "🍌"
+                emoji_banana_amount += 1
             if amt == 1:
                 message = f"You eat {amt} banana {banana_string}!"
             else:
                 message = f"You eat {amt} bananas {banana_string}!"
             await remove_bananas(user_id, amt)
-            await interaction.response.send_message(f"{message}\n{banana_gif}")
-            return
+            if interaction.response.is_done():
+                await interaction.followup.send(f"{message}\n{banana_gif}")
+            else:
+                await interaction.response.send_message(f"{message}\n{banana_gif}")
+
 
