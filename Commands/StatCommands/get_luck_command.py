90b08f9e6f45ad6e4bd79d93a7ffd0ee67bd8024
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-05-03 19:35:51 -0400

    updated fucntion name

--- Commands/StatCommands/get_luck_command.py
+++ Commands/StatCommands/get_luck_command.py
@@ -8,7 +8,7 @@ from utils.emoji_helper import BANANA_COIN_EMOJI
 
 async def define_get_luck_command(tree, servers):
     @tree.command(name="get_luck", description="Get your current Luck stat", guilds=servers)
-    async def get_luck(interaction: discord.Interaction):
+    async def get_luck_command(interaction: discord.Interaction):
         user_id = interaction.user.id
         current_luck = await get_luck(str(user_id))
         await interaction.response.send_message(f"Your current luck is: {current_luck}!", ephemeral=True)
