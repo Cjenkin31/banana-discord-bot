5384e519a5fdfd637ec44bd855c01469a0b238bb
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-05-03 19:36:55 -0400

    sent user id through

--- Commands/StatCommands/reroll_luck_command.py
+++ Commands/StatCommands/reroll_luck_command.py
@@ -10,19 +10,19 @@ async def define_reroll_luck_command(tree, servers):
     @tree.command(name="reroll_luck", description="Reroll your luck for currency!", guilds=servers)
     @app_commands.describe(amount="Amount of bananas to spend on rerolling luck")
     async def reroll_luck(interaction: discord.Interaction, amount: int):
-        user_id = interaction.user.id
+        user_id = str(interaction.user.id)
 
         if amount <= 0:
             await interaction.response.send_message("You need to spend a positive number of bananas.", ephemeral=True)
             return
 
-        user_bananas = await get_bananas(str(user_id))
+        user_bananas = await get_bananas(user_id)
 
         if user_bananas >= amount:
-            await remove_bananas(str(user_id), amount)
-            luck_to_add = await random_luck(amount)
+            await remove_bananas(user_id, amount)
+            luck_to_add = await random_luck(user_id, amount)
             
-            await set_luck(str(user_id), luck_to_add)
+            await set_luck(user_id, luck_to_add)
 
             await interaction.response.send_message(f"You have rerolled and set your luck to {luck_to_add} for {amount} {BANANA_COIN_EMOJI}!", ephemeral=True)
         else:
