fe121b5de998c0b3b6d57280b15ec895152a69c9
Jacob Waclawski <54681308+jmw3638@users.noreply.github.com>
2024-05-04 10:47:40 -0400

    fix royal check

--- game/poker/royal_flush.py
+++ game/poker/royal_flush.py
@@ -2,9 +2,9 @@ from game.poker.straight_flush import StraightFlush
 
 class RoyalFlush(StraightFlush):
     def makes_hand(self, hand):
-        super().makes_hand(hand)
-
-        return self.ranks[0] == 'A'
+        if super().makes_hand(hand):
+            return self.ranks[0] == 'A'
+        return False
 
     def get_hand_cards(self):
         return super().get_hand_cards()
