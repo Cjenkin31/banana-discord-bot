14bd756f7ca302db62db1a911d78b4423aa0a4c8
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-05-17 23:33:54 -0400

    added debug line

--- data/firebase_message.py
+++ data/firebase_message.py
@@ -10,6 +10,7 @@ async def set_message_mapping(original_message_id, forwarded_message_id):
 
 async def get_message_mapping(original_message_id):
     ref = db.reference(f'messages/{original_message_id}')
+    print(ref.get())
     return ref.get()
 
 async def remove_message_mapping(original_message_id):
