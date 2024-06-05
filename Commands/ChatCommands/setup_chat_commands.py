29dc4cd044cdc8919b4e36d907416eb2cfe05e74
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-05 19:14:48 -0400

    removed a test command

--- Commands/ChatCommands/setup_chat_commands.py
+++ Commands/ChatCommands/setup_chat_commands.py
@@ -4,9 +4,7 @@ from discord import app_commands
 from Commands.ChatCommands.ask_bread_command import define_ask_bread_command
 from Commands.ChatCommands.eat_banana_command import define_eat_banana_command
 from Commands.ChatCommands.summarize_youtube import define_summarize_youtube_video_command
-from Commands.ChatCommands.send_multiple_messages import define_multiple_messages_command
 async def define_all_chat_commands(tree, servers):
     await define_ask_bread_command(tree, servers)
     await define_eat_banana_command(tree, servers)
     await define_summarize_youtube_video_command(tree, servers)
-    await define_multiple_messages_command(tree, servers)
