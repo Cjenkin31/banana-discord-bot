746898fcd8db020823f9cefcaf425325d1d54a53
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-07 18:11:46 -0400

    updated daily to give more flavourful responses

--- Commands/CurrencyCommands/daily_currency_command.py
+++ Commands/CurrencyCommands/daily_currency_command.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from discord import app_commands
 import discord
 from utils.emoji_helper import BANANA_COIN_EMOJI
+from utils.gpt import generate_gpt_response
 
 async def define_daily_command(tree, servers):
     @tree.command(name="daily", description="Collect your daily bananas", guilds=servers)
@@ -15,5 +16,10 @@ async def define_daily_command(tree, servers):
             formatted_wait_time = f"{wait_time.seconds // 3600} hours and {(wait_time.seconds // 60) % 60} minutes"
             await interaction.response.send_message(f"Please wait {formatted_wait_time} to collect your daily bananas.")
         else:
+            await interaction.response.defer()
             bananas_collected = result
-            await interaction.response.send_message(f"You collected {bananas_collected} {BANANA_COIN_EMOJI}! Come back in 24 hours for more.")
+            model = "gpt-4o"
+            story = f"You are a narrator talking about how somebody came across money. You do this in 1 or 2 lines and always mention the user along with how many banana coins, you can also use the emoji {BANANA_COIN_EMOJI}"
+            user_input = f"{interaction.user.mention} collected {bananas_collected} bananas."
+            response_message = await generate_gpt_response(model, story, user_input)
+            await interaction.followup.send(response_message)
