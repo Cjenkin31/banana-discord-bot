5020504beeb481ef3f516d4c5da813adaf54896d
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-06 23:41:23 -0400

    added a technical comment

--- utils/message_utils.py
+++ utils/message_utils.py
@@ -2,7 +2,7 @@ import discord
 
 DISCORD_MESSAGE_LIMIT = 2000
 CHUNK_SIZE = 2000
-
+# Requires the callee to have a deffer interaction response, i.e await interaction.response.defer()
 async def send_message_in_chunks(response_string: str, interaction: discord.Interaction):
     if len(response_string) > DISCORD_MESSAGE_LIMIT - 1:
         chunks = [response_string[i:i + CHUNK_SIZE] for i in range(0, len(response_string), CHUNK_SIZE)]
