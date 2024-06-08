1c9e4fcf590eb4bb164908e1ab7b16543721a32f
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-07 22:49:28 -0400

    trying an opensource downloader class

--- Commands/VoiceCommands/play_command.py
+++ Commands/VoiceCommands/play_command.py
@@ -9,15 +9,124 @@ import uuid
 from moviepy.editor import AudioFileClip
 from utils.audio_queue import AudioQueue
 from pytube.exceptions import VideoUnavailable, PytubeError
-from typing import List
-
-# TODO: Support for playlists
-# TODO: New command for playing audio from links. Soundcloud, Spotify, etc.
+from typing import List, Union
+from concurrent.futures import ThreadPoolExecutor
+from yt_dlp import YoutubeDL
 
 audio_queue = AudioQueue()
 MAX_DOWNLOAD_SONGS_AT_A_TIME = 3  # Define the maximum number of simultaneous downloads
 
 class Downloader:
+    __YDL_OPTIONS = {'format': 'bestaudio/best',
+                     'default_search': 'auto',
+                     'playliststart': 0,
+                     'extract_flat': False,
+                     'quiet': True,
+                     'ignore_no_formats_error': True
+                     }
+    __YDL_OPTIONS_EXTRACT = {'format': 'bestaudio/best',
+                             'default_search': 'auto',
+                             'playliststart': 0,
+                             'extract_flat': True,
+                             'quiet': True,
+                             'ignore_no_formats_error': True
+                             }
+    __YDL_OPTIONS_FORCE_EXTRACT = {'format': 'bestaudio/best',
+                                   'default_search': 'auto',
+                                   'playliststart': 0,
+                                   'extract_flat': False,
+                                   'quiet': True,
+                                   'ignore_no_formats_error': True
+                                   }
+    __BASE_URL = 'https://www.youtube.com/watch?v={}'
+
+    def __init__(self) -> None:
+        self.__music_keys_only = ['resolution', 'fps', 'quality']
+        self.__not_extracted_keys_only = ['ie_key']
+        self.__not_extracted_not_keys = ['entries']
+        self.__playlist_keys = ['entries']
+
+    def finish_one_song(self, song: str) -> dict:
+        try:
+            if song is None:
+                return None
+
+            if self.is_url(song):
+                song_info = self.__download_url(song)
+            else:
+                song_info = self.__download_title(song)
+
+            return song_info
+        except Exception as e:
+            print(f"Error downloading song: {e}")
+            raise
+
+    @staticmethod
+    def is_url(url: str) -> bool:
+        return url.startswith("http")
+
+    def __get_forced_extracted_info(self, url: str) -> dict:
+        options = Downloader.__YDL_OPTIONS_FORCE_EXTRACT
+        with YoutubeDL(options) as ydl:
+            try:
+                extracted_info = ydl.extract_info(url, download=False)
+                return extracted_info
+            except Exception as e:
+                print(f'Error forcing extract: {e}')
+                return {}
+
+    def __download_url(self, url) -> dict:
+        options = Downloader.__YDL_OPTIONS
+        with YoutubeDL(options) as ydl:
+            try:
+                result = ydl.extract_info(url, download=False)
+                return result
+            except Exception as e:
+                print(f'Error downloading URL: {e}')
+                return {}
+
+    def __download_title(self, title: str) -> dict:
+        options = Downloader.__YDL_OPTIONS
+        with YoutubeDL(options) as ydl:
+            try:
+                search = f'ytsearch:{title}'
+                extracted_info = ydl.extract_info(search, download=False)
+
+                if self.__failed_to_extract(extracted_info):
+                    extracted_info = self.__get_forced_extracted_info(title)
+
+                if extracted_info is None:
+                    return {}
+
+                if self.__is_multiple_musics(extracted_info):
+                    if len(extracted_info['entries']) == 0:
+                        return {}
+                    return extracted_info['entries'][0]
+                else:
+                    print(f'Failed to extract title: {title}')
+                    return {}
+            except Exception as e:
+                print(f'Error downloading title: {e}')
+                return {}
+
+    def __is_multiple_musics(self, extracted_info: dict) -> bool:
+        for key in self.__playlist_keys:
+            if key not in extracted_info.keys():
+                return False
+        return True
+
+    def __failed_to_extract(self, extracted_info: dict) -> bool:
+        if type(extracted_info) is not dict:
+            return False
+
+        for key in self.__not_extracted_keys_only:
+            if key not in extracted_info.keys():
+                return False
+        for key in self.__not_extracted_not_keys:
+            if key in extracted_info.keys():
+                return False
+        return True
+
     async def download_song(self, url: str, guild_id: int) -> str:
         try:
             yt = YouTube(url)
@@ -154,10 +263,10 @@ async def define_play_youtube_audio_command(tree, servers):
                 print(f"An error occurred in download_with_retry: {e}")
                 raise
 
-def remove_file_if_exists(file_path: str):
-    try:
-        if os.path.exists(file_path):
-            os.remove(file_path)
-            print(f"File removed: {file_path}")
-    except Exception as e:
-        print(f"Failed to remove file {file_path}: {e}")
+    def remove_file_if_exists(file_path: str):
+        try:
+            if os.path.exists(file_path):
+                os.remove(file_path)
+                print(f"File removed: {file_path}")
+        except Exception as e:
+            print(f"Failed to remove file {file_path}: {e}")
