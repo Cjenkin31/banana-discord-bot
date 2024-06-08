ac6e90d1eeeb5dc4bf50e2e1035a8533699a75a6
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-07 21:01:31 -0400

    defined setup command

--- Commands/VoiceCommands/setup_voice_commands.py
+++ Commands/VoiceCommands/setup_voice_commands.py
@@ -1,7 +1,8 @@
-from Commands.VoiceCommands.skip_command import define_skip_youtube_audio_command
-from Commands.VoiceCommands.stop_command import define_stop_youtube_audio_command
 from openai import OpenAI
 import os
+from Commands.VoiceCommands.set_join_to_create_vc_command import define_set_join_to_create_vc
+from Commands.VoiceCommands.skip_command import define_skip_youtube_audio_command
+from Commands.VoiceCommands.stop_command import define_stop_youtube_audio_command
 from Commands.VoiceCommands.play_command import define_play_youtube_audio_command
 from Commands.VoiceCommands.speak_command import define_speak_command
 from Commands.VoiceCommands.cleanup_vc_command import define_cleanup_vc_command
@@ -15,3 +16,4 @@ async def define_all_voice_commands(tree, servers, elevenlabskey):
     await define_play_youtube_audio_command(tree, servers)
     await define_stop_youtube_audio_command(tree, servers)
     await define_skip_youtube_audio_command(tree, servers)
+    await define_set_join_to_create_vc(tree, servers)
