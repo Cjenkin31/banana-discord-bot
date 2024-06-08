dfc9b0c6cfdbe0e2073a52a4204b41ac0246bfc1
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-07 20:28:04 -0400

    changed to asyncio

--- utils/audio_queue.py
+++ utils/audio_queue.py
@@ -1,45 +1,47 @@
-import threading
+import asyncio
 
 class AudioQueue:
     _instance = None
-    _lock = threading.Lock()
 
     def __new__(cls):
-        with cls._lock:
-            if cls._instance is None:
-                cls._instance = super(AudioQueue, cls).__new__(cls)
-                cls._instance.queues = {}
+        if cls._instance is None:
+            cls._instance = super(AudioQueue, cls).__new__(cls)
+            cls._instance.queues = {}
+            cls._instance._lock = asyncio.Lock()
         return cls._instance
 
-    def get_queue(self, guild_id):
-        with self._lock:
+    async def get_queue(self, guild_id):
+        async with self._instance._lock:
             if guild_id not in self._instance.queues:
                 self._instance.queues[guild_id] = []
             return self._instance.queues[guild_id]
 
-    def add_to_queue(self, guild_id, track_info):
-        with self._lock:
-            self.get_queue(guild_id).append(track_info)
+    async def add_to_queue(self, guild_id, track_info):
+        async with self._instance._lock:
+            queue = await self.get_queue(guild_id)
+            queue.append(track_info)
 
-    def next_track(self, guild_id):
-        with self._lock:
-            queue = self.get_queue(guild_id)
+    async def next_track(self, guild_id):
+        async with self._instance._lock:
+            queue = await self.get_queue(guild_id)
             if queue:
                 return queue.pop(0)
 
-    def show_queue(self, guild_id):
-        with self._lock:
-            return self.get_queue(guild_id)
+    async def show_queue(self, guild_id):
+        async with self._instance._lock:
+            return await self.get_queue(guild_id)
 
-    def clear_queue(self, guild_id):
-        with self._lock:
+    async def clear_queue(self, guild_id):
+        async with self._instance._lock:
             if guild_id in self._instance.queues:
                 self._instance.queues[guild_id] = []
 
-    def is_queue_empty(self, guild_id):
-        with self._lock:
-            return len(self.get_queue(guild_id)) == 0
+    async def is_queue_empty(self, guild_id):
+        async with self._instance._lock:
+            queue = await self.get_queue(guild_id)
+            return len(queue) == 0
 
-    def queue_length(self, guild_id):
-        with self._lock:
-            return len(self.get_queue(guild_id))
+    async def queue_length(self, guild_id):
+        async with self._instance._lock:
+            queue = await self.get_queue(guild_id)
+            return len(queue)
