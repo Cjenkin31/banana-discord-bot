ebeac351b6171177daea3e4d46442d2e63d923a3
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-07 22:59:53 -0400

    better handling

--- Commands/VoiceCommands/play_command.py
+++ Commands/VoiceCommands/play_command.py
@@ -125,22 +125,29 @@ async def define_play_youtube_audio_command(tree, servers):
     async def play_audio(voice_client, guild_id, interaction, process_task):
         while True:
             track_info = await audio_queue.next_track(guild_id)
-            if not track_info:
+            if track_info is None:
                 if process_task.done() and await audio_queue.is_queue_empty(guild_id):
                     break
                 await asyncio.sleep(1)  # Wait a bit before checking the queue again
                 continue
 
-            track = track_info["file"]
-            track_url = track_info["url"]
-            print(f"Now playing: {track_url}")
-            voice_client.play(FFmpegPCMAudio(executable="ffmpeg", source=track))
-            await interaction.channel.send(f"Now playing: {track_url}")
-            while voice_client.is_playing():
-                await asyncio.sleep(1)
-            remove_file_if_exists(track)
+            if voice_client and voice_client.is_connected():
+                track = track_info["file"]
+                track_url = track_info["url"]
+                print(f"Now playing: {track_url}")
+                voice_client.play(FFmpegPCMAudio(executable="ffmpeg", source=track))
+                await interaction.channel.send(f"Now playing: {track_url}")
+                while voice_client.is_playing():
+                    await asyncio.sleep(1)
+                remove_file_if_exists(track)
         
-        await voice_client.disconnect()
+            else:
+                print("Voice client not connected or is None")
+                break
+        
+        if voice_client:
+            await voice_client.disconnect()
+
 
     async def download_with_retry(url: str, guild_id: int, max_retries=3):
         for attempt in range(max_retries):
