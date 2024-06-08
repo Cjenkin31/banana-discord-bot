63c43036a64d35dba8a1503dfdbfb9de4d9756e4
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-07 20:09:46 -0400

    added thread safety to audio queue

--- utils/audio_queue.py
+++ utils/audio_queue.py
@@ -1,28 +1,45 @@
+import threading
+
 class AudioQueue:
     _instance = None
+    _lock = threading.Lock()
 
     def __new__(cls):
-        if cls._instance is None:
-            cls._instance = super(AudioQueue, cls).__new__(cls)
-            cls._instance.queues = {}
+        with cls._lock:
+            if cls._instance is None:
+                cls._instance = super(AudioQueue, cls).__new__(cls)
+                cls._instance.queues = {}
         return cls._instance
 
     def get_queue(self, guild_id):
-        if guild_id not in self._instance.queues:
-            self._instance.queues[guild_id] = []
-        return self._instance.queues[guild_id]
+        with self._lock:
+            if guild_id not in self._instance.queues:
+                self._instance.queues[guild_id] = []
+            return self._instance.queues[guild_id]
 
     def add_to_queue(self, guild_id, track_info):
-        self.get_queue(guild_id).append(track_info)
+        with self._lock:
+            self.get_queue(guild_id).append(track_info)
 
     def next_track(self, guild_id):
-        queue = self.get_queue(guild_id)
-        if queue:
-            return queue.pop(0)
+        with self._lock:
+            queue = self.get_queue(guild_id)
+            if queue:
+                return queue.pop(0)
 
     def show_queue(self, guild_id):
-        return self.get_queue(guild_id)
+        with self._lock:
+            return self.get_queue(guild_id)
 
     def clear_queue(self, guild_id):
-        if guild_id in self._instance.queues:
-            self._instance.queues[guild_id] = []
+        with self._lock:
+            if guild_id in self._instance.queues:
+                self._instance.queues[guild_id] = []
+
+    def is_queue_empty(self, guild_id):
+        with self._lock:
+            return len(self.get_queue(guild_id)) == 0
+
+    def queue_length(self, guild_id):
+        with self._lock:
+            return len(self.get_queue(guild_id))
