e9f49468f5881602168d1dba42e8c69c3d6d3286
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-09 15:03:46 -0400

    removed the shop command

--- Commands/ShopCommands/setup_shop_commands.py
+++ Commands/ShopCommands/setup_shop_commands.py
@@ -3,9 +3,9 @@ from discord.ext import commands
 from discord import app_commands
 from .random_timeout_command import define_random_timeout_command
 from .assassinate_command import define_assassinate_command
-from .shop_command import define_shop_command
+# from .shop_command import define_shop_command
 
 async def define_all_shop_commands(tree, servers):
     await define_random_timeout_command(tree, servers)
     await define_assassinate_command(tree, servers)
-    await define_shop_command(tree, servers)
+    # await define_shop_command(tree, servers)
