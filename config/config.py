dc20929048a433318234d679026c12850e4c4082
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-12 20:38:44 -0400

    chore: Update Discord bot configuration and environment variables

--- config/config.py
+++ config/config.py
@@ -1,13 +1,25 @@
 import os
 import discord
 from openai import OpenAI
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # Server Ids
 OVERBOTCHED_ID = discord.Object(id=1101665956314501180)
 BONK_BRIGADE_ID = discord.Object(id=1210021401772429352)
+TEST_SERVER_ID = discord.Object(id=844017098022715402)
 
-# Discord Token + Intents
-TOKEN = os.getenv('BOT_TOKEN')
+environment = os.getenv('ENV', 'DEVELOPMENT')
+if environment == "PRODUCTION":
+    TOKEN = os.getenv('BOT_TOKEN')
+    SERVERS = [OVERBOTCHED_ID, BONK_BRIGADE_ID]
+    debug_mode = False
+else:
+    TOKEN = os.getenv('DEVELOPMENT_BOT_TOKEN')
+    SERVERS = [TEST_SERVER_ID]
+    debug_mode = True
+
 INTENTS = discord.Intents.default()
 INTENTS.messages = True
 INTENTS.reactions = True
@@ -15,7 +27,7 @@ INTENTS.members = True
 INTENTS.message_content = True
 INTENTS.voice_states = True
 
-SERVERS = [OVERBOTCHED_ID, BONK_BRIGADE_ID]
-
-GPTKEY = os.environ.get('OPENAI_API_KEY')
+ELEVEN_LABS_API_KEY = os.getenv('xi-api-key')
+FIREBASE_SERVICE_ACCOUNT = os.getenv('FIREBASE_SERVICE_ACCOUNT')
+GPTKEY = os.getenv('OPENAI_API_KEY')
 CLIENT = OpenAI(api_key=GPTKEY)
--- config/firebase_config.py
+++ config/firebase_config.py
@@ -1,3 +1,4 @@
+from config.config import FIREBASE_SERVICE_ACCOUNT
 import firebase_admin
 from firebase_admin import credentials, db
 import os
@@ -5,7 +6,7 @@ import json
 
 def initialize_firebase():
     if not firebase_admin._apps:
-        service_account_info = json.loads(os.getenv('FIREBASE_SERVICE_ACCOUNT'))
+        service_account_info = json.loads(FIREBASE_SERVICE_ACCOUNT)
         cred = credentials.Certificate(service_account_info)
         firebase_admin.initialize_app(cred, {
             'databaseURL': os.getenv("FIREBASE_DATABASE_URL")
