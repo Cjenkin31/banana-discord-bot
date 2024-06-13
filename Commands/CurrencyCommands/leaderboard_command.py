8488503d79ae82c07af695551ab4417cd6d421b0
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-13 17:20:20 -0400

    refactor: Handle missing user in leaderboard_command

--- Commands/CurrencyCommands/leaderboard_command.py
+++ Commands/CurrencyCommands/leaderboard_command.py
@@ -15,13 +15,18 @@ async def define_leaderboard_command(tree, servers):
             embed = discord.Embed(title="Top Users by Bananas", description="Here are the top banana earners:", color=0xf1c40f)
             if thumbnail_file:
                 embed.set_thumbnail(url="attachment://image.jpg")
-            
             for index, (user_id, amount) in enumerate(leaderboard_data[:10], start=1):
-                user = await interaction.client.fetch_user(user_id)
-                embed.add_field(name=f"{index}. {BANANA_COIN_EMOJI} {user.display_name}", value=f"{amount}", inline=False)
+                try:
+                    user = await interaction.client.fetch_user(user_id)
+                except discord.errors.NotFound:
+                    user = "Unknown User"
+                if isinstance(user, discord.User):
+                    embed.add_field(name=f"{index}. {BANANA_COIN_EMOJI} {user.display_name}", value=f"{amount}", inline=False)
+                else:
+                    embed.add_field(name=f"{index}. {BANANA_COIN_EMOJI} {user}", value=f"{amount}", inline=False)
             if thumbnail_file:
                 await interaction.response.send_message(embed=embed, file=thumbnail_file)
             else:
                 await interaction.response.send_message(embed=embed)
         except Exception as e:
-            await interaction.response.send_message(f"Failed to display leaderboard: {str(e)}", ephemeral=True)
+            await interaction.followup.send_message(f"Failed to display leaderboard: {str(e)}", ephemeral=True)
