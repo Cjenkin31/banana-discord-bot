0c92ba64131a6f52f7a58681141748af875e2378
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-12 20:57:26 -0400

    feat: Add set_nickname command for banana bread activities

---
+++ Commands/ChatCommands/nickname_command.py
@@ -0,0 +1,13 @@
+from GPT_stories import getStoryByRole
+import discord
+from discord.ext import commands
+from discord import app_commands
+from utils.gpt import generate_gpt_response
+from utils.message_utils import send_message_in_chunks
+from utils.users import UNBUTTERED_BAGEL_ID
+from data.nickname import set_nickname as nn
+async def define_set_nickname_command(tree, servers):
+    @tree.command(name="set_nickname", description="Sets your nickname for all banana bread related activities", guilds=servers)
+    async def set_nickname(interaction: discord.Interaction, user_input: str):
+        new_nickname = nn(interaction.user.id, user_input)
+        await interaction.response.send_message(f"Your nickname has been set to {new_nickname}.")
