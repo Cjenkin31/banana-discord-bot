0f909c581ee6ad9cd37b101c678fab73886c9ef9
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-06-26 19:31:52 -0400

    fix: updated file name along with turning it into a cog

--- cogs/Commands/ChatCommands/get_auth_token_command.py
+++ cogs/Commands/ChatCommands/get_auth_token_command.py
@@ -1,21 +1,26 @@
 import uuid
+from config.config import SERVERS
 import discord
 from discord.ext import commands
 from discord import app_commands
 import logging
 from data.auth import get_auth_token, set_auth_token
 
-async def dm_user(user, message):
-    try:
-        await user.send(message)
-    except discord.Forbidden:
-        logging.error("Failed to send DM: Check the user's privacy settings.")
-        return False
-    return True
+class AuthTokenCog(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    async def dm_user(self, user, message):
+        try:
+            await user.send(message)
+        except discord.Forbidden:
+            logging.error("Failed to send DM: Check the user's privacy settings.")
+            return False
+        return True
 
-async def define_auth_token_command(tree, servers):
-    @tree.command(name="get_auth_token", description="Get your authentication token", guilds=servers)
-    async def get_auth_token_command(interaction: discord.Interaction):
+    @app_commands.command(name="get_auth_token", description="Get your authentication token")
+    @app_commands.guilds(*SERVERS)
+    async def get_auth_token_command(self, interaction: discord.Interaction):
         await interaction.response.defer(ephemeral=True)
         user_id = interaction.user.id
         token = await get_auth_token(user_id)
@@ -27,9 +32,12 @@ async def define_auth_token_command(tree, servers):
                 return
             token = new_token
         
-        message = f"Here is your userID\n```{user_id}```\nHere is your authentication token( This is specific to banana bread activites ):\n ```{token}```\nKeep it safe!"
+        message = f"Here is your userID\n```{user_id}```\nHere is your authentication token (This is specific to banana bread activities):\n ```{token}```\nKeep it safe!"
         
-        if not await dm_user(interaction.user, message):
+        if not await self.dm_user(interaction.user, message):
             await interaction.followup.send("I couldn't send you a DM. Please check your privacy settings and try again.", ephemeral=True)
         else:
             await interaction.followup.send("I've sent you your authentication token via DM!", ephemeral=True)
+
+async def setup(bot):
+    await bot.add_cog(AuthTokenCog(bot))
