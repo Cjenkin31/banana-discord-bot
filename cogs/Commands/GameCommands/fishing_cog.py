1cb0c676bf3f02ae23ec088123d18f9e74d64997
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-07-05 18:44:32 -0400

    fix: updated the fishing command to send back a response

--- cogs/Commands/GameCommands/fishing_cog.py
+++ cogs/Commands/GameCommands/fishing_cog.py
@@ -11,19 +11,10 @@ class FishingCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def process_askbread(self, user_id, user_input, role):
-        story = getStoryByRole(role, user_id)
-        model = "gpt-4o"
-        response_message = await generate_gpt_response(model, story, user_input)
-        return response_message
-
     @app_commands.command(name="fishing", description="Fishing Game!")
     @app_commands.guilds(*SERVERS)
     async def askbread(self, interaction: discord.Interaction):
-        await interaction.response.defer()
-        response = await generate_gpt_response("gpt3.5-turbo", "You are a fisherman who only speaks in words of fish names. Please only send back 1 word.", "Give me a random fish")
-        await interaction.followup.send(f"Fishing is WIP.....Pretend to have recieved a cool fish....Actually here is a gpt generated fish name. {response}")
-        # await send_message_in_chunks(response_message, interaction)
+        await interaction.channel.send("This is still under development.... Please be patient")
 
 async def setup(bot):
     await bot.add_cog(FishingCog(bot))
