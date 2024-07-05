1951437ce822f27f99a2536c21c6f463189994e4
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-07-05 18:52:24 -0400

    chore: udpated how I send back a response

--- cogs/Commands/GameCommands/fishing_cog.py
+++ cogs/Commands/GameCommands/fishing_cog.py
@@ -14,7 +14,7 @@ class FishingCog(commands.Cog):
     @app_commands.command(name="fishing", description="Fishing Game!")
     @app_commands.guilds(*SERVERS)
     async def askbread(self, interaction: discord.Interaction):
-        await interaction.channel.send("This is still under development.... Please be patient")
+        await interaction.response.send_message("This is still under development.... Please be patient")
 
 async def setup(bot):
     await bot.add_cog(FishingCog(bot))
