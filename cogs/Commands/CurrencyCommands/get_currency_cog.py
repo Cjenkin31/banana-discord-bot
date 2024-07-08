f8ea1c5555ef3294b0046a05a33055f259da157a
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-07-08 18:06:21 -0400

    fix: wrong tabbing

--- cogs/Commands/CurrencyCommands/get_currency_cog.py
+++ cogs/Commands/CurrencyCommands/get_currency_cog.py
@@ -9,13 +9,13 @@ class GetCurrencyCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-@commands.command(name="get_bananas", help="Get your banana coin amount", aliases=['bal','balance', 'cash', 'get_cash', 'get_currency'])
-async def get_currency(self, ctx):
-    try:
-        amount = await get_bananas(str(ctx.author.id))
-        await ctx.send(f"You have {amount} {BANANA_COIN_EMOJI}.")
-    except Exception as e:
-        await ctx.send(f"Failed to get currency: {str(e)}")
+    @commands.command(name="get_bananas", help="Get your banana coin amount", aliases=['bal','balance', 'cash', 'get_cash', 'get_currency'])
+    async def get_currency(self, ctx):
+        try:
+            amount = await get_bananas(str(ctx.author.id))
+            await ctx.send(f"You have {amount} {BANANA_COIN_EMOJI}.")
+        except Exception as e:
+            await ctx.send(f"Failed to get currency: {str(e)}")
 
     @app_commands.guilds(*SERVERS)
     @app_commands.command(name="get_bananas", description="Get your banana coin amount")
