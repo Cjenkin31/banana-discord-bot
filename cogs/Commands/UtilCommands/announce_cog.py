98fddf5573525a248367920e823a5ffffe8f7b21
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-07-12 19:51:41 -0400

    fix: wrong type

--- cogs/Commands/UtilCommands/announce_cog.py
+++ cogs/Commands/UtilCommands/announce_cog.py
@@ -14,44 +14,38 @@ class AnnouncementCog(commands.Cog):
             return interaction.user.id == UNBUTTERED_BAGEL_ID
         return app_commands.check(predicate)
 
-    @app_commands.command(name="announce", description="...")
+    @app_commands.command(name="announce", description="Send an announcement to the specified channels")
     @app_commands.guilds(*SERVERS)
     @is_owner()
     async def announce(self, interaction: discord.Interaction, user_input: str, channel_ids: str = ""):
         await interaction.response.defer()
-        print(channel_ids)
         if not channel_ids:
-            print("getting all channels")
-            channel_ids = await get_all_announcement_channels()
-            print(channel_ids)
+            channel_dict = await get_all_announcement_channels()
+            channel_ids = list(channel_dict.values())
         else:
-            print("splitting channel ids")
             channel_ids = [int(id.strip()) for id in channel_ids.split(",") if id.strip().isdigit()]
-            print(channel_ids)
+
         for channel_id in channel_ids:
-            print(channel_id)
             channel = self.bot.get_channel(int(channel_id))
-            print(f"Channel: Right after getting: {channel}")
             if channel is None:
                 try:
-                    print("channel is none")
                     channel = await self.bot.fetch_channel(int(channel_id))
                 except discord.NotFound:
-                    await interaction.response.send_message(f"Channel with ID {channel_id} not found.", ephemeral=True)
+                    await interaction.followup.send(f"Channel with ID {channel_id} not found.", ephemeral=True)
                     continue
                 except discord.Forbidden:
-                    await interaction.response.send_message(f"Bot does not have access to the channel with ID {channel_id}.", ephemeral=True)
+                    await interaction.followup.send(f"Bot does not have access to the channel with ID {channel_id}.", ephemeral=True)
                     continue
                 except discord.HTTPException as e:
-                    await interaction.response.send_message(f"Failed to fetch channel due to an HTTP error: {e}", ephemeral=True)
+                    await interaction.followup.send(f"Failed to fetch channel due to an HTTP error: {e}", ephemeral=True)
                     continue
-            print(f"Channel = {channel}")
+
             if channel:
-                print("sending message")
                 await channel.send(user_input)
             else:
-                await interaction.response.send_message(f"Failed to send message to channel {channel_id}", ephemeral=True)
-            await interaction.followup.send("Message sent to all channels", ephemeral=True)
+                await interaction.followup.send(f"Failed to send message to channel {channel_id}", ephemeral=True)
+
+        await interaction.followup.send("Message sent to all channels", ephemeral=True)
 
 async def setup(bot):
     await bot.add_cog(AnnouncementCog(bot))
