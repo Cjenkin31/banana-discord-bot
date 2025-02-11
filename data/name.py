478112181e40ba52a635a13b08990a8450a83d34
Christopher Jenkin <christopherjenkin64@gmail.com>
2025-02-11 16:34:10 -0500

    feat: update name validation logic in add_name_if_not_exist_to_database function

--- data/name.py
+++ data/name.py
@@ -16,6 +16,6 @@ async def add_name_if_not_exist_to_database(user_id, name):
     ref = db.reference(f'users/{user_id}')
     user_name_ref = ref.child('user_name')
     user_name = user_name_ref.get()
-    if not user_name:
+    if name != user_name:
         user_name_ref.set(name)
         get_name_sync.cache_clear()
